578e23b29b51bea85a0fd636f9a905f4204ad62c
asketola <annemarie.s.ketola@gmail.com>
2015-08-16 23:04:53 -0700

    Moved some code around to make it more efficient

--- EKAlarmExample/ViewController.swift
+++ EKAlarmExample/ViewController.swift
@@ -7,12 +7,62 @@
 //
 
 import UIKit
+import EventKit
+
+var calendarDatabase = EKEventStore()
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var reminderDatePicker: UIDatePicker!
+    @IBOutlet weak var reminderTItle: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        calendarDatabase.requestAccessToEntityType(EKEntityTypeReminder, completion: { (granted: Bool, error:NSError!) -> Void in
+            if !granted {
+                println("Access to store not granted")
+            } else {
+                println("We have alarm permission")
+            }
+        })
+    }
+    
+    // changed the location of this function to above
+//    func eventStoreAccessReminders() {
+//        calendarDatabase.requestAccessToEntityType(EKEntityTypeReminder, completion: { (granted: Bool, error:NSError!) -> Void in
+//            if !granted {
+//                println("Access to store not granted")
+//            }
+//        })
+//    }
+    
+    func accessCalendarInTheDatabase() {
+        var calendars = calendarDatabase.calendarsForEntityType(EKEntityTypeReminder)
+        
+        for calendar in calendars as! [EKCalendar] {
+            println("Calendar = \(calendar.title)")
+        }
+    }
+    
+    func createReminder(reminderTitle: String, reminderDate: NSDate) {
+        let reminder = EKReminder(eventStore: calendarDatabase)
+        
+        reminder.title = reminderTItle.text
+        let date = reminderDate
+        let alarm = EKAlarm(absoluteDate: date)
+        
+        reminder.addAlarm(alarm)
+        
+        reminder.calendar = calendarDatabase.defaultCalendarForNewReminders()
+        
+        var error = NSError?()
+        
+        if error != nil {
+            println("errors: \(error?.localizedDescription)")
+        }
+        
+        calendarDatabase.saveReminder(reminder, commit: true, error: &error)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +70,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func setReminder(sender: AnyObject) {
+        
+        calendarDatabase.requestAccessToEntityType(EKEntityTypeReminder, completion: { (granted, error) -> Void in
+            if !granted {
+                println("Access to store not granted")
+                println("\(error.localizedDescription)")
+            } else {
+                println("Reminder entered: \(self.reminderTItle.text), \(self.reminderDatePicker.date)")
+            }
+            
+        })
+        createReminder(reminderTItle.text, reminderDate: reminderDatePicker.date)
+        
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        reminderTItle.endEditing(true)
+    }
 
 }
-
